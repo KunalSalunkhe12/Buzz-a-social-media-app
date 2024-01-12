432751aaad70bfe6065c0c291612f004d6908c9f
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2024-01-12 16:18:12 +0530

    Add Loader component and useGetCurrentUser hook in Saved page

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,13 +1,15 @@
 import GridPostList from "@/components/shared/GridPostList";
-import { useUserContext } from "@/context/user/UserContext";
+import Loader from "@/components/shared/Loader";
+import { useGetCurrentUser } from "@/lib/react-query/queries";
 
 const Saved = () => {
-  const { user } = useUserContext();
+  const { data: user, isSuccess, isPending } = useGetCurrentUser();
   return (
     <div className="flex flex-1 justify-center overflow-y-scroll custom-scrollbar pb-6">
       <div className="w-full md:w-1/2 flex flex-col gap-10">
         <h2 className="text-sm md:text-xl font-semibold">Saved Posts</h2>
-        <GridPostList posts={user.savedPosts} />
+        {isSuccess && <GridPostList posts={user.savedPosts} />}
+        {isPending && <Loader />}
       </div>
     </div>
   );
